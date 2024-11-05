b1c50f0e5ac49b73fe0db6f585a0eccb41ce6586
DaniloSovano <danilosovano@gmail.com>
2024-11-05 17:05:09 -0300

    Add files via upload

--- jogo_da_velha.c
+++ jogo_da_velha.c
@@ -3,12 +3,24 @@
 int main() {
     int mat[3][3] = { {0} };  
 
-    char letras[3] = {'A', 'B', 'C'};
-
+    char linhas[3] = {'A', 'B', 'C'};
+    int colunas[3] = {1,2,3};
+    
+    
+    printf("  ");
+    for (int i = 0; i < 3; i++) {
+        printf(" %d  ", colunas[i]);
+    
+    }   
+    printf("\n");
     for (int j = 0; j < 3; j++) {
-        printf("%c ", letras[j]);
+        
+        printf("%c ", linhas[j]);
+
         for (int k = 0; k < 3; k++) {
+            
             printf("|%d| ", mat[j][k]);
+          
             }
         printf("\n");
             
